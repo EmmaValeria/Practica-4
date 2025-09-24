fdedfd838b21b6c5310aa33115036e2b28cff90c
goretti <gorettichavarria20@gmail.com>
2025-09-23 19:29:56 -0600

    feat: actualizar HiloProceso

--- src/Proceso/HiloProceso.java
+++ src/Proceso/HiloProceso.java
@@ -11,6 +11,45 @@ public class HiloProceso extends Thread {
     }
     @Override
     public void run() {
+        for (int i = 0; i < 5 ; i++) {
+            System.out.println("Proceso "+nombre+"| tamanio:  "+tamanio+"| prioridad: "+prioridad);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            if(i == 2){
+                Thread.yield();
+            }
+        }
+        System.out.println("Proceso Terminado");
+    }
+
+    public static void main(String[] args) {
+        Thread chrome = new Thread(new HiloProceso("Chrome",500,7));
+        Thread word = new Thread(new HiloProceso("Word",200,5));
+        Thread antivirus = new Thread(new HiloProceso("Antivirus",800,10));
+        Thread spotify = new Thread(new HiloProceso("Spotify",300,4));
+        Thread explorer = new Thread(new HiloProceso("Explorer",700,8));
+
+        chrome.start();
+        try {
+            chrome.join();
+        }catch (InterruptedException e){e.printStackTrace();}
+
+        word.start();
+        try {
+            word.join();
+        }catch (InterruptedException e){e.printStackTrace();}
+
+        antivirus.setPriority(Thread.MAX_PRIORITY);
+        antivirus.start();
+        try {
+            antivirus.join();
+        }catch (InterruptedException e){e.printStackTrace();}
+
 
     }
+
+
 }
